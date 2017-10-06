e6271e1cc0e1b8f08c60ae5f40df9bbc690e255b
Toljas <hopeinyourself2011@gmail.com>
2017-10-06 20:10:06 +0300

    done homework 6.10.2017

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function timeToNewYear() {
+//timeCelebration =[YYYY, M, D]
+//M = 0 for January, 1 for February...
+function timeToNewYear(timeCelebration, nameCelebration) {
   const now = new Date();
-  const newYear = new Date(2018, 1, 1);
-  const diff = newYear.getTime() - now.getTime();
-  console.log(`There are ${diff/1000} s to New Year`);
+  const holiday = new Date(timeCelebration[0],timeCelebration[1], timeCelebration[2]);
+  const diff = holiday.getTime() - now.getTime();
+  console.log(`There are
+  ${Math.trunc(diff/(1000*3600*24))} days
+  ${Math.trunc((diff/(1000*3600*24)-Math.trunc(diff/(1000*3600*24)))*24)} hours
+  ${Math.trunc((diff/(1000*3600)-Math.trunc(diff/(1000*3600)))*60)} minutes
+  ${Math.trunc((diff/(1000*60)-Math.trunc(diff/(1000*60)))*60)} seconds
+  ${Math.trunc((diff/(1000)-Math.trunc(diff/(1000)))*1000)} milliseconds
+to ${nameCelebration}`);
 }
-const intervalid = setInterval(timeToNewYear, 1000);
-setTimeout(() => clearInterval(intervalid), 10000);
+timeToNewYear([2018, 1, 15],`Anniversary 1 year`);
