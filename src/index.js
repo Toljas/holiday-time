592d7cda616eae4efe7d2677101c71ae0ba38877
Toljas <hopeinyourself2011@gmail.com>
2017-10-08 18:35:10 +0300

    Made first simple server with html file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-const file = fs.readFileSync('public/index.html');
+  const file = fs.readFileSync('public/index.html');
   res.end(file);
 });
 
-server.listen(3000, () => {console.log('Listening on port 3000')});
+server.listen(3000, () => {
+  console.log('Listening on port 3000')
+});
